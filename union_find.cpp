b988c9fb3e066ab9e23fd7943683324ad80b94aa
Jaewon Chung <jaywonchung@snu.ac.kr>
2018-08-09 10:58:15 +0900

    add new snippet

---
+++ union_find.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+
+int parent[100];
+
+int Find(int x) {
+    if (x==parent[x]) {
+        return x;
+    } else {
+        return parent[x] = Find(parent[x]);
+    }
+}
+
+int Union(int x, int y) {
+    parent[Find[x]] = Find(y);
+}
+
+int main()
+{
+    // n = max number of elements
+    int n; scanf("%d", &n);
+    
+    // initialize parent array
+    for (int i=0; i<n; ++i) {
+        parent[i] = i;
+    }
+    
+    // union a and b
+    Union(a, b);
+    
+    // find the parent of a
+    cout << Find(a) << '\n';
+    
+    // chcek of a and b are in the same set
+    if (Find(a)==Find(b))
+    
+    
+    return 0;
+}
